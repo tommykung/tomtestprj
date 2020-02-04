3316b67c6a67d351c0b7900b7440edd341d8659b
NOPPADOL.SU <noppadol.sun@glsict.com>
2020-02-04 16:15:20 +0700

    Edit new service

--- tom_service/newservice.js
+++ tom_service/newservice.js
@@ -1,19 +1,2 @@
-const express = require('express');
-const app = express();
-const port = 10001;
-
-app.get('/patient', function(req, res){
-	console.log("enter patient");
-	res.json({ mesage: "enter patient res"});
-});
-
-
-// app.get('/', (req, res) => {
-//   res.json({ message: 'temp!' })
-// })
-
-
-app.listen(port, () => {
-	console.log("Application is running on port "+port);
-});
+console.log("new service ");
 
